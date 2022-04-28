630c803eaf4786ef6c65a300f01a228fc21e53dc
Nick <n.carlson.design@gmail.com>
2022-04-27 20:08:26 -0700

    Update ContactForm.tsx

--- src/components/forms/ContactForm.tsx
+++ src/components/forms/ContactForm.tsx
@@ -13,12 +13,24 @@ const ContactForm = () => {
               action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSdvSKii47RBrkk2wV-PswPqKk3ZqiJR7jWtaS_sq3ESmZdfeA/formResponse"
               method="post"
             >
-              <label className="white-text">Name</label>
-              <input type="text" id="entry.1630247408" name="name" required />
-              <label className="white-text">Email</label>
-              <input type="email" id="email" name="entry.1807431491" required />
-              <label className="white-text">How Can I Help?</label>
-              <textarea name="entry.314039078" rows={5} required />
+              <Label>Name</Label>
+              <Input
+                type="text"
+                id="entry.1630247408"
+                name="name"
+                placeholder="Your Name"
+                required
+              />
+              <Label>Email</Label>
+              <Input
+                type="email"
+                id="email"
+                name="entry.1807431491"
+                placeholder="Your Email"
+                required
+              />
+              <Label>How Can I Help?</Label>
+              <TextArea name="entry.314039078" rows={5} required />
               <Button
                 text="Submit"
                 style={{ width: "fit-content" }}
@@ -37,6 +49,33 @@ const FormWrapper = styled.div.attrs({
 })`
   background: rgba(0, 0, 0, 0.66);
   padding: 40px 40px 60px 40px;
+`;
+
+const Label = styled.label`
+  color: white;
+  margin-top: 24px;
+  margin-bottom: 16px;
+`;
+
+const Input = styled.input`
+  padding: 12px;
+  background-color: #202833;
+  border: none;
+
+  &:focus-visible {
+    outline: 1px solid #66fcf1;
+  }
+`;
+
+const TextArea = styled.textarea`
+  padding: 12px;
+  background-color: #202833;
+  border: none;
+  margin-bottom: 24px;
+
+  &:focus-visible {
+    outline: 1px solid #66fcf1;
+  }
 `;
 
 const Form = styled.form`
